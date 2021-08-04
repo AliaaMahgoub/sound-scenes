6ad780dd35ce1d6bf228d9451bfaaae8ebd48700
AliaaMahgoub <aliamahgoub2005@gmail.com>
2021-08-04 15:47:13 -0400

    2 layer neural network combinations, bar chart (size 1, size2), powers of 2 from 2-2048

--- NN2.py
+++ NN2.py
@@ -6,42 +6,53 @@ from sklearn import preprocessing
 
 X = np.load('/home/aliaamahgoub/X.npy')
 y = np.load('/home/aliaamahgoub/y.npy')
-size_list = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-#layers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+l1_size_list = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+#l1_size_list = [2, 4, 8]
+l2_size_list = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+#l2_size_list = [2, 4, 8]
 
 X_ts, y_ts, X_train_folds, y_train_folds = split_data(X,y)
 
 Nfolds = len(X_train_folds)
-yerr = []
+#yerr = []
 y = []
+labels = []
+#j = 1
+
 #for num_layers in layers:
-for size in size_list:
-    print("current hidden size is: ", size)
-    scores = []
-    tr_scores = []
+for size_1 in l1_size_list:
+    for size_2 in l2_size_list:
+        print("current hidden size is: ", size_1, size_2)
+        scores = []
+        tr_scores = []
+    
+        for i in range(Nfolds):
+            X_vl = X_train_folds[i]
+            y_vl = y_train_folds[i]
+            X_tr = np.vstack([X_train_folds[j] for j in range(Nfolds) if j != i])
+            y_tr = np.hstack([y_train_folds[j] for j in range(Nfolds) if j != i])
+            scaler = preprocessing.StandardScaler().fit(X_tr)
+            X_tr_sc = scaler.transform(X_tr)
+            X_vl_sc = scaler.transform(X_vl)
+            clf = MLPClassifier(solver='lbfgs', max_iter=300,
+                    hidden_layer_sizes=(size_1, size_2))
+            clf.fit(X_tr_sc, y_tr)
+            scores.append(clf.score(X_vl_sc,y_vl))
+            tr_scores.append(clf.score(X_tr_sc,y_tr))
+        print("validation accuracy was ", np.mean(scores))
+        print("training accuracy was ", np.mean(tr_scores))
+        #j += 1
+        curr_lab = str(size_1)+','+str(size_2)
+        labels.append(curr_lab)
+        y.append(np.mean(scores))
+        #yerr.append(np.std(scores))
 
-    for i in range(Nfolds):
-        X_vl = X_train_folds[i]
-        y_vl = y_train_folds[i]
-        X_tr = np.vstack([X_train_folds[j] for j in range(Nfolds) if j != i])
-        y_tr = np.hstack([y_train_folds[j] for j in range(Nfolds) if j != i])
-        scaler = preprocessing.StandardScaler().fit(X_tr)
-        X_tr_sc = scaler.transform(X_tr)
-        X_vl_sc = scaler.transform(X_vl)
-        clf = MLPClassifier(solver='lbfgs', max_iter=300,
-                hidden_layer_sizes=(size))
-        clf.fit(X_tr_sc, y_tr)
-        scores.append(clf.score(X_vl_sc,y_vl))
-        tr_scores.append(clf.score(X_tr_sc,y_tr))
-    print("validation accuracy was ", np.mean(scores))
-    print("training accuracy was ", np.mean(tr_scores))
-    y.append(np.mean(scores))
-    yerr.append(np.std(scores))
-
-plt.errorbar(size_list, y, yerr=yerr)
+#plt.errorbar(size_list, y, yerr=yerr)
+plt.rc('axes', axisbelow=True)
 plt.grid()
-plt.xlabel('hidden layer size')
+plt.xlabel('hidden layer sizes (layer 1, layer 2)')
+plt.bar(labels ,y, color = 'tab:cyan')
 plt.ylabel('CV accuracy')
 plt.ylim([0,1])
-plt.savefig("NN_cv_all.png")
+plt.savefig("NN2_cv_all.png")
 plt.clf()
